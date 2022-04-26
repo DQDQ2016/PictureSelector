d9e00b176d72560924cfe66a58c39b091a45fb11
DQDQ <pruplelightningtrex@gmail.com>
2022-04-26 23:54:15 +0800

    图片选择和显示

--- app/src/main/java/com/dqdq/pictureslector/MainActivity.kt
+++ app/src/main/java/com/dqdq/pictureslector/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.dqdq.pictureslector
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+import android.graphics.BitmapFactory
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val permissionRequestCode = 2568
+    var pictureSelector: PictureSelector? = null
+    private val RC_LOCATION_CONTACTS_PERM = 124
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pictureSelector = findViewById(R.id.pic_selector)
+        pictureSelector?.setPictureSelectListener {
+            openSysAlbum()
+        }
+
+        var permission =
+            ActivityCompat.checkSelfPermission(this, "android.permission.WRITE_EXTERNAL_STORAGE");
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), permissionRequestCode
+            );
+        }
+    }
+
+    /**
+     * 打开系统相册
+     */
+    private fun openSysAlbum() {
+        val albumIntent = Intent(Intent.ACTION_PICK)
+        albumIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+        startActivityForResult(
+            albumIntent,
+            RC_LOCATION_CONTACTS_PERM
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == RC_LOCATION_CONTACTS_PERM) {
+                val uri = data!!.data
+                var path = getPhotoFromPhotoAlbum.getRealPathFromUri(this, uri)
+                val bitmap = BitmapFactory.decodeFile(path)
+                pictureSelector?.pushPicture(path,bitmap)
+            }
+        }
     }
 }
--- app/src/main/java/com/dqdq/pictureslector/PictureSelector.kt
+++ app/src/main/java/com/dqdq/pictureslector/PictureSelector.kt
@@ -3,10 +3,7 @@ package com.dqdq.pictureslector
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Resources
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import android.util.AttributeSet
@@ -14,7 +11,6 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import androidx.annotation.RequiresApi
-import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import java.util.*
 
 /**
@@ -23,14 +19,17 @@ import java.util.*
 class PictureSelector : View {
 
     private var redPointRadius = 20f
-    private var maxItemCountPerLayer = 3
-    private var pictureCount = 3
+    private var maxItemCountPerLayer = 3 //每层最多item
+    private var pictureCount = 5 //总item
     private var itemWidth = 200
     private var itemTopMargin = 40
     private var cancelStrokeWidth = 4f
     private var xPadding = 10
     private var items = PriorityQueue<SelectorItem>()
     private lateinit var itemsPoint: MutableList<Array<Float>>
+    private var listener: (() -> Unit)? = null
+    private var picCount = 0 //记录已选择的图片数量
+    //todo 图片显示顺序逻辑
 
     constructor(context: Context) : super(context)
 
@@ -46,45 +45,27 @@ class PictureSelector : View {
         itemsPoint = MutableList(pictureCount) { arrayOf(0f, 0f) }
 
         var compareByUrl = Comparator<SelectorItem> { t, t1 ->
-            Log.i("test","compare")
+            Log.i("test", "compare")
             if (t.imgUrl == "" && t1.imgUrl != "")
+                 1
+            else if (t.imgUrl != "" && t1.imgUrl == "")
                 -1
-            else if(t.imgUrl != "" && t1.imgUrl == "")
-                1
-            else if(t.imgUrl == "" && t1.imgUrl == "")
-                t.index - t1.index
-            else if(t.imgUrl != "" && t1.imgUrl != "")
+            else if (t.imgUrl != "" && t1.imgUrl != "")
                 t.index - t1.index
             else
-                0
+                t.index - t1.index
         }
 
         items = PriorityQueue<SelectorItem>(compareByUrl)
-
-        val res: Resources = resources
-        var r =  res.getDrawable(R.drawable.one)
-        var db:BitmapDrawable = r as BitmapDrawable
 
         var margin: Float =
             (width - (itemWidth * maxItemCountPerLayer)) / (2 + (maxItemCountPerLayer - 1)).toFloat()
         var x = margin
         var y = 50f
-        for (i in 0 until pictureCount) {
-            itemsPoint[i][0] = x
-            itemsPoint[i][1] = y
-
-            if (i == 0) {
-                items.add(SelectorItem(x, y, i, "", db.bitmap))
-            } else if (i == 1) {
-                r =  res.getDrawable(R.drawable.two)
-                db = r as BitmapDrawable
-                items.add(SelectorItem(x, y, i, "", db.bitmap))
-            } else if (i == 2) {
-                r =  res.getDrawable(R.drawable.three)
-                db = r as BitmapDrawable
-                items.add(SelectorItem(x, y, i, "", db.bitmap))
-            }else
-                items.add(SelectorItem(x, y, i, "", null))
+        for (i in 1..pictureCount) {
+            itemsPoint[i - 1][0] = x
+            itemsPoint[i - 1][1] = y
+            items.add(SelectorItem(x, y, i, "", null))
 
             if (i % maxItemCountPerLayer == 0 && i != 0) {
                 x = margin
@@ -99,42 +80,43 @@ class PictureSelector : View {
         super.onDraw(canvas)
         var paint = Paint()
 
-        for ((index,item) in items.withIndex()) {
+        for ((index, item) in items.withIndex()) {
             item.x = itemsPoint[index][0]
             item.y = itemsPoint[index][1]
-
-            with(item) {
-
-                if (item.imgUrl != null) {
-                    item.imgBitmap?.let {
-                        canvas?.drawBitmap(it,x,y,paint)
-                    }
-                    paint.color = Color.RED
-                    canvas?.drawCircle(x + itemWidth, y, redPointRadius, paint)
-                    paint.color = Color.WHITE//画X
-                    paint.strokeWidth = cancelStrokeWidth
-
-                    var redPointX = x + itemWidth - redPointRadius
-                    var redPointY = y - redPointRadius
-
-                    canvas?.drawLine(
-                        redPointX + xPadding,
-                        redPointY + xPadding,
-                        redPointX + redPointRadius * 2 - xPadding,
-                        redPointY + redPointRadius * 2 - xPadding,
-                        paint
-                    )
-                    canvas?.drawLine(
-                        redPointX + redPointRadius * 2 - xPadding,
-                        redPointY + xPadding,
-                        redPointX + xPadding,
-                        redPointY + redPointRadius * 2 - xPadding,
-                        paint
-                    )
-                }else{
-                    paint.color = Color.BLACK
-                    canvas?.drawRect(x, y, (x + itemWidth), (y + itemWidth), paint) //todo 绘制背景图
+            if (item.imgUrl != null && item.imgUrl != "") {
+                item.imgBitmap?.let {
+                    canvas?.drawBitmap(it, item.x, item.y, paint)
                 }
+                paint.color = Color.RED
+                canvas?.drawCircle(item.x + itemWidth, item.y, redPointRadius, paint)
+                paint.color = Color.WHITE//画X
+                paint.strokeWidth = cancelStrokeWidth
+
+                var redPointX = item.x + itemWidth - redPointRadius
+                var redPointY = item.y - redPointRadius
+
+                canvas?.drawLine(
+                    redPointX + xPadding,
+                    redPointY + xPadding,
+                    redPointX + redPointRadius * 2 - xPadding,
+                    redPointY + redPointRadius * 2 - xPadding,
+                    paint
+                )
+                canvas?.drawLine(
+                    redPointX + redPointRadius * 2 - xPadding,
+                    redPointY + xPadding,
+                    redPointX + xPadding,
+                    redPointY + redPointRadius * 2 - xPadding,
+                    paint
+                )
+            } else {
+                val res: Resources = resources
+                var r = res.getDrawable(R.drawable.comment_addimg)
+                var db: BitmapDrawable = r as BitmapDrawable
+                var bgBitmap = zoomImage(db.bitmap,itemWidth,itemWidth)
+                paint.color = Color.BLACK
+                canvas?.drawBitmap(bgBitmap,item.x, item.y ,paint)
+                break
             }
         }
     }
@@ -149,8 +131,7 @@ class PictureSelector : View {
                 itemCanCelClickEvent(item)
                 break
             } else if (event!!.x in (item.x..item.x + itemWidth)
-                && event!!.y in (item.y..item.y + itemWidth)
-            ) {
+                && event!!.y in (item.y..item.y + itemWidth) && item.imgUrl == "") {
                 itemClickEvent(item)
                 break
             }
@@ -169,14 +150,68 @@ class PictureSelector : View {
 
     private fun itemClickEvent(item: SelectorItem) { //todo 选择图片
         Log.i("test", "itemClickEvent ${item.x} ${item.y}")
+        listener?.invoke()
     }
 
     private fun itemCanCelClickEvent(item: SelectorItem) {
         items.remove(item)
-        item.imgUrl = null
+        item.imgUrl = ""
         item.imgBitmap = null
         items.add(item)
         invalidate()
         Log.i("test", "itemCanCelClickEvent ${item.x} ${item.y}")
     }
+
+    fun setPictureSelectListener(listener: () -> Unit) {
+        this.listener = listener
+    }
+
+    fun pushPicture(url: String, pic: Bitmap) {
+        var item: SelectorItem? = null
+        for (v in items) {
+            if (v.imgUrl == null || v.imgUrl == "") {
+                item = v
+                break
+            }
+        }
+        if (item != null) {
+            var newBitmap = zoomImage(pic, itemWidth, itemWidth)
+            items.remove(item)
+            item.imgUrl = url
+            item.imgBitmap = newBitmap
+            var tempList = mutableListOf<SelectorItem>()
+            tempList.add(item)
+            tempList.addAll(items)
+            items.clear()
+            items.addAll(tempList)
+            invalidate()
+        }
+        Log.i("test", "pushPicture")
+    }
+
+    fun getPictures(): List<SelectorItem> {
+        return items.toList()
+    }
+
+    private fun zoomImage(
+        bgImage: Bitmap, newWidth: Int,
+        newHeight: Int
+    ): Bitmap {
+        // 获取这个图片的宽和高
+        var width = bgImage.width;
+        var height = bgImage.height;
+        // 创建操作图片用的matrix对象
+        var matrix = Matrix()
+        // 计算宽高缩放率
+        var scaleWidth = (newWidth.toFloat()) / width;
+        var scaleHeight = (newHeight.toFloat()) / height;
+        // 缩放图片动作
+        matrix.postScale(scaleWidth, scaleHeight);
+        var bitmap = Bitmap.createBitmap(
+            bgImage, 0, 0, width,
+            height, matrix, true
+        );
+        return bitmap;
+    }
+
 }
